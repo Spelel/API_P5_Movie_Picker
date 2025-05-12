6b467ccfb09b0049cbdba767e6fd8a292a33240b
Arek <arek-karczo.wp.pl@wp.pl>
2025-05-12 20:44:21 +0200

    test for input value to see if it's working

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
-// fetch("http://www.omdbapi.com/?i=tt3896198&apikey=e802e6a9")
-//     .then(res => res.json())
-//     .then(data => console.log(data))
+// fetch("http://www.omdbapi.com/?i=tt3896198&apikey=e802e6a9&t=star+wars")
+
+const btnSearch = document.getElementById("btnSearch")
+let movieArray =[]
+
+function getMovie() {
+    const txtInput = document.getElementById("textInput")
+
+    if (txtInput.value === ''){
+        console.log('empty space')
+    } else {
+        fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=e802e6a9&s=${txtInput.value}`)
+            .then(res => res.json())
+            .then(data => {
+        console.log(data)
+        movieArray = data.Search
+        
+        movieArray.forEach(item => {
+            console.log(item.Title)
+        })
+    })
+    }
+    // txtInput.value = ''
+}
+
+
+
+btnSearch.addEventListener("click", () => {
+    getMovie()
+})
